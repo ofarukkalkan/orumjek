5be532b90fdd1f1bde61d1d467b1371956db3b13
ofarukkalkan <wanteddewil@outlook.com>
2017-06-02 20:58:26 +0300

    proje silindi

--- arama_motoru/arama_motoru-web/src/main/java/com/orumjek/managed_bean/PageCtrLocal.java
+++ arama_motoru/arama_motoru-web/src/main/java/com/orumjek/managed_bean/PageCtrLocal.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.orumjek.managed_bean;
+package com.orumjek.sesion_bean;
+
+import javax.ejb.Local;
 
 /**
  *
  * @author omerfaruk
  */
+@Local
 public interface PageCtrLocal {
 
     public void create(String url, String htmlData, String textData, double score);
